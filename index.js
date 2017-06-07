5ad9a4b674b80bbaacb99f3d7b8bea9eaca9f687
sgrigory <grisha.sizov@gmail.com>
2017-06-07 17:45:11 +0200

    0

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/', function(request, response) {
   response.render('pages/test');
 });
 
+
+app.get('/france', function(request, response) {
+  response.render('pages/leaflet');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
